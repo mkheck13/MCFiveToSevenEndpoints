5c73fdf3b6a51be42e32b255c4a3b5ee6494b40c
mkheck13 <mkheck13@gmail.com>
2025-01-08 13:40:57 -0800

    reverse it num is working

--- Controllers/ReverseItNumController.cs
+++ Controllers/ReverseItNumController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MCFiveToSevenEndpoints.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MCFiveToSevenEndpoints.Controllers
@@ -10,6 +11,18 @@ namespace MCFiveToSevenEndpoints.Controllers
     [Route("[controller]")]
     public class ReverseItNumController : ControllerBase
     {
-        
+        private readonly ReverseItNumServices _reverseItNumServices;
+
+        public ReverseItNumController(ReverseItNumServices reverseItNumServices)
+        {
+            _reverseItNumServices = reverseItNumServices;
+        }
+
+        [HttpGet]
+        [Route("ReverseNum/")]
+        public string ReverseNum(string input)
+        {
+            return _reverseItNumServices.ReverseNum(input);
+        }
     }
 }
--- Services/ReverseItNumServices.cs
+++ Services/ReverseItNumServices.cs
@@ -1,7 +1,26 @@
+using Microsoft.AspNetCore.Components.Forms;
+
 namespace MCFiveToSevenEndpoints.Services
 {
     public class ReverseItNumServices
     {
-        
+        public string ReverseNum(string input)
+        {
+            int revNumOut = 0;
+            bool isTrue = int.TryParse(input, out int num);
+            if(isTrue)
+            {
+                while(num != 0)
+                {
+                    revNumOut = revNumOut * 10 + (num % 10);
+                    num = num / 10;
+                }
+            }
+            else
+            {
+                return "Please enter a number and try again.";
+            }
+            return $"{input} reversed is {revNumOut}";
+        }
     }
 }
